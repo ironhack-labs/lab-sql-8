c2595c5326b24534e4e9a7a184da724a6eee0dda
Ines Fonseca <ines_filipa_11@hotmail.com>
2023-03-18 21:21:47 +0000

    Lab 8

--- LAB 8.sql
+++ LAB 8.sql
@@ -22,36 +22,44 @@ where length > 0 AND length is not null;
 #Hint: Use appropriate join between the tables "category" and "film_category".
 
 select
- category.name, count(*) as film_count
-from film_category a
-join category on a.category_id = category.category_id
-group by category.name;
+c.name, COUNT(*) as film_count
+from CATEGORY c
+join FILM_CATEGORY f on (c.category_id = f.category_id)
+group by c.name
+order by film_count desc
 ;
 
 # 4 Which actor has appeared in the most films? Hint: You can create a join between the tables "actor" and "film actor" and 
 #count the number of times an actor appears.
 
-select first_name, last_name, a.actor_id, count(fa.film_id) as number_films
+select
+a.actor_id,a.first_name, a.last_name, COUNT(f.film_id) as actor_count
 from actor a
-join film_actor fa on a.actor_id = fa.actor_id
+join film_actor f on (a.actor_id = f.actor_id)
 group by a.actor_id
-order by number_films desc limit 1;
+order by actor_count desc
+limit 1;
+
 
 #5 - Which is the most active customer (the customer that has rented the most number of films)? 
 #Hint: Use appropriate join between the tables "customer" and "rental" and count the rental_id for each customer.
 
-select count(c.customer_id) as rentals_count,f.last_name,f.first_name 
-from sakila.rental c
-join sakila.customer f on c.customer_id=f.customer_id 
-group by last_name 
-order by rentals_count desc limit 1;
+select
+c.customer_id, c.first_name, c.last_name, count(*) as active_customer
+from customer c
+join rental r on (c.customer_id = r.customer_id)
+group by c.customer_id
+order by active_customer desc limit 1;
+
 
 #Bonus: Which is the most rented film? (The answer is Bucket Brotherhood).
 
-select f.title, count(*) as rental_count 
-from sakila.film f
-    join sakila.inventory i on f.film_id=i.film_id
-    join sakila.rental r on i.inventory_id=r.inventory_id
-    group by f.film_id order by rental_count desc limit 1;
 
+select i.film_id, f.title,count(*) as most_rented_film
+from inventory i
+join film f on (i.film_id = f.film_id)
+join rental r on (i.inventory_id = r.inventory_id)
+group by i.film_id
+order by most_rented_film desc limit 1
+;
 

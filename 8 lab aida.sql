710d0203fd09278e0c7a16e1c38a6a1cf27d19a5
aidagarcd <aidagarcd@gmail.com>
2024-01-08 18:33:15 +0100

    aida lab 8 mysql

--- 8 lab aida.sql
+++ 8 lab aida.sql
@@ -1,36 +1,34 @@
+-- Rank films by length (filter out the rows with nulls or zeros in length column). 
+     -- Select only columns title, length and rank in your output.
+-- Rank films by length within the rating category (filter out the rows with nulls or zeros in length column).
+     -- In your output, only select the columns title, length, rating and rank.
+-- How many films are there for each of the categories in the category table? 
+     -- Hint: Use appropriate join between the tables "category" and "film_category".
+-- Which actor has appeared in the most films? 
+     -- Hint: You can create a join between the tables "actor" and "film actor" and count the number of times an actor appears.
+-- Which is the most active customer (the customer that has rented the most number of films)? 
+     -- Hint: Use appropriate join between the tables "customer" and "rental" and count the rental_id for each customer.
+
 use sakila;
 
-SELECT title, length, RANK() OVER (ORDER BY length) AS
+-- 1 and 2:
+SELECT title, length, RANK() OVER (ORDER BY length) AS ranking FROM film WHERE length IS NOT NULL AND length != 0 ORDER BY length;
 
-SELECT title, length, rating, RANK() OVER (PARTITION BY rating ORDER BY length) AS rank
-FROM film_view
-ORDER BY rating, rank;
+SELECT c.category_id, c.name AS category_name, COUNT(fc.film_id) OVER (PARTITION BY c.category_id) AS film_count FROM category c
+LEFT JOIN film_category fc ON c.category_id = fc.category_id
+ORDER BY category_name;
 
-select a.name as category, count(*) as 'Film_count'
-from category a
-join film_category fc on a.category_id = fc.category_id
-group by c.name;
-​
-select a.actor_id, a.first_name, a.last_name, count(*) as film_count
-from actor a
-join film_actor fa on a.actor_id = fa.actor_id
-group by a.actor_id
-order by film_count desc limit 1;
-​
+SELECT a.actor_id, a.first_name, a.last_name AS most_appeared_actor, COUNT(fa.actor_id) AS film_count
+FROM actor a
+LEFT JOIN film_actor fa ON a.actor_id = fa.actor_id
+GROUP BY a.actor_id, a.first_name, a.last_name
+ORDER BY film_count DESC
+LIMIT 1;
 
-select c.customer_id, c.first_name, c.last_name, count(*) as rental_count
-from customer c
-join rental r on c.customer_id = r.customer_id
-group by c.customer_id, c.first_name, c.last_name
-order by rental_count desc limit 1;
-
-
-
-
-
-
-
-
-
-
+SELECT c.customer_id, c.first_name, c.last_name AS best_customer, COUNT(r.rental_id) AS rental_count
+FROM customer c
+LEFT JOIN rental r ON c.customer_id = r.customer_id
+GROUP BY c.customer_id, c.first_name, c.last_name
+ORDER BY rental_count DESC
+LIMIT 1;
 
